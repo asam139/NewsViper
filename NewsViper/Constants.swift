2b18a7c997ef3c1a554fc81031da52f38e0f2ad6
Saul Moreno Abril <93sauu@gmail.com>
2018-09-08 12:01:47 +0200

    Add Constants adding api url

---
+++ NewsViper/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NewsViper
+//
+//  Created by Saul Moreno Abril on 08/09/2018.
+//  Copyright © 2018 Saul Moreno Abril. All rights reserved.
+//
+
+import Foundation
+
+
+class Constants {
+    public static let apiURL = "http://feeds.bbci.co.uk/news/world/rss.xml";
+}
