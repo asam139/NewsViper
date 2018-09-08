b4ea429230da0c90a8710f0e9d3fd09ec72d096b
Saul Moreno Abril <93sauu@gmail.com>
2018-09-08 10:22:07 +0200

    Initial setup

---
+++ NewsViper/modules/view/NewsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewsViewController.swift
+//  NewsViper
+//
+//  Created by Saul Moreno Abril on 08/09/2018.
+//  Copyright © 2018 Saul Moreno Abril. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
